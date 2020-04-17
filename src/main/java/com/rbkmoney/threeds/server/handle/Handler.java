4cee384932e63be819574aba492135c2c620b42d
karleowne <a.karlov@rbkmoney.com>
2020-04-17 13:02:20 +0700

    BJ-871: remove library classes

--- src/main/java/com/rbkmoney/threeds/server/handle/Handler.java
+++
@@ -1,8 +0,0 @@
-package com.rbkmoney.threeds.server.handle;
-
-public interface Handler<T, R> {
-
-    boolean canHandle(T o);
-
-    R handle(T o);
-}
--- src/main/java/com/rbkmoney/threeds/server/handle/RequestHandler.java
+++
@@ -1,7 +0,0 @@
-package com.rbkmoney.threeds.server.handle;
-
-import com.rbkmoney.threeds.server.domain.root.Message;
-
-public interface RequestHandler extends Handler<Message, Message> {
-
-}
