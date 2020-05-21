494679778c84b7c1375c55c69c82def987a971a5
a.romanov <a.romanov@rbkmoney.com>
2020-05-21 14:52:45 +0300

    BJ-907: Create PResToRBKMoneyPreparationResponseHandlerImpl

---
+++ src/main/java/com/rbkmoney/threeds/server/handle/impl/PResToRBKMoneyPreparationFlowResponseHandlerImpl.java
@@ -0,0 +1,23 @@
+package com.rbkmoney.threeds.server.handle.impl;
+
+import com.rbkmoney.threeds.server.config.DirectoryServerProviderHolder;
+import com.rbkmoney.threeds.server.domain.root.Message;
+import com.rbkmoney.threeds.server.domain.root.emvco.PRes;
+import com.rbkmoney.threeds.server.dto.ValidationResult;
+import com.rbkmoney.threeds.server.processor.Processor;
+import com.rbkmoney.threeds.server.service.MessageValidatorService;
+
+public class PResToRBKMoneyPreparationFlowResponseHandlerImpl extends ResponseHandlerImpl {
+
+    public PResToRBKMoneyPreparationFlowResponseHandlerImpl(
+            Processor<ValidationResult, Message> processor,
+            MessageValidatorService validator,
+            DirectoryServerProviderHolder providerHolder) {
+        super(processor, validator, providerHolder);
+    }
+
+    @Override
+    public boolean canHandle(Message message) {
+        return message instanceof PRes;
+    }
+}
