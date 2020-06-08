9bd176511a73ab24906a98fc899df529e96a4363
karleowne <a.karlov@rbkmoney.com>
2020-06-08 20:03:30 +0700

    BJ-935: refactor properties
    add different endpoints for card ranges and transaction info
    replace preparation on platform

--- src/main/java/com/rbkmoney/threeds/server/ThreeDsServerApplication.java
+++ src/main/java/com/rbkmoney/threeds/server/ThreeDsServerApplication.java
@@ -3,10 +3,7 @@ package com.rbkmoney.threeds.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.util.stream.Stream;
 
 @EnableScheduling
 @ServletComponentScan
@@ -14,12 +11,6 @@ import java.util.stream.Stream;
 public class ThreeDsServerApplication extends SpringApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(ThreeDsServerApplication.class, args);
-        Stream.of(applicationContext.getBeanDefinitionNames())
-                .sorted()
-                .filter(s -> s.indexOf("Processor") != -1)
-                .filter(s -> s.indexOf("springframework") == -1)
-                .filter(s -> s.indexOf("Chain") != -1)
-                .forEach(s -> System.out.println(s));
+        SpringApplication.run(ThreeDsServerApplication.class, args);
     }
 }
