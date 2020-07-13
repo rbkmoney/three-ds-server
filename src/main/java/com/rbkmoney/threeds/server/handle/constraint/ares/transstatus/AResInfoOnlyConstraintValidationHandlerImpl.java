d444ae19521bbf3ef513df1004dd3977cba88fdb
karleowne <a.karlov@rbkmoney.com>
2020-07-13 14:41:56 +0700

    BJ-923: add visa tests

--- src/main/java/com/rbkmoney/threeds/server/handle/constraint/ares/transstatus/AResInfoOnlyConstraintValidationHandlerImpl.java
+++ src/main/java/com/rbkmoney/threeds/server/handle/constraint/ares/transstatus/AResInfoOnlyConstraintValidationHandlerImpl.java
@@ -5,6 +5,7 @@ import com.rbkmoney.threeds.server.domain.root.emvco.ARes;
 import com.rbkmoney.threeds.server.domain.threedsrequestor.ThreeDSRequestorChallengeInd;
 import com.rbkmoney.threeds.server.dto.ConstraintValidationResult;
 import com.rbkmoney.threeds.server.handle.constraint.ares.AResConstraintValidationHandler;
+import com.rbkmoney.threeds.server.holder.DirectoryServerProviderHolder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,8 @@ import static com.rbkmoney.threeds.server.utils.Wrappers.getValue;
 @Component
 @RequiredArgsConstructor
 public class AResInfoOnlyConstraintValidationHandlerImpl implements AResConstraintValidationHandler {
+
+    private final DirectoryServerProviderHolder providerHolder;
 
     @Override
     public boolean canHandle(ARes o) {
@@ -28,10 +31,15 @@ public class AResInfoOnlyConstraintValidationHandlerImpl implements AResConstrai
 
         if (!(threeDSRequestorChallengeInd == ThreeDSRequestorChallengeInd.NO_CHALLENGE_RISK_ANALYSIS_PERFORMED
                 || threeDSRequestorChallengeInd == ThreeDSRequestorChallengeInd.NO_CHALLENGE_DATA_SHARE_ONLY
-                || threeDSRequestorChallengeInd == ThreeDSRequestorChallengeInd.NO_CHALLENGE_AUTH_ALREADY_PERFORMED)) {
+                || threeDSRequestorChallengeInd == ThreeDSRequestorChallengeInd.NO_CHALLENGE_AUTH_ALREADY_PERFORMED
+                || isVisaCrutch(threeDSRequestorChallengeInd))) {
             return ConstraintValidationResult.failure(PATTERN, "transStatus");
         }
 
         return ConstraintValidationResult.success();
+    }
+
+    private boolean isVisaCrutch(ThreeDSRequestorChallengeInd threeDSRequestorChallengeInd) {
+        return threeDSRequestorChallengeInd == ThreeDSRequestorChallengeInd.RESERVED_FOR_DS_USED_82 && providerHolder.getEnvironmentProperties().getThreeDsServerRefNumber().equals("3DS_LOA_SER_DIPL_020200_00236");
     }
 }
