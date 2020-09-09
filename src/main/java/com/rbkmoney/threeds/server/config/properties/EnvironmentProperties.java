c6186e87828bde13b0642f480ee77c39676015c7
karleowne <a.karlov@rbkmoney.com>
2020-09-09 17:20:16 +0700

    BJ-985: updating the configuration of deprecated tests

--- src/main/java/com/rbkmoney/threeds/server/config/properties/EnvironmentProperties.java
+++ src/main/java/com/rbkmoney/threeds/server/config/properties/EnvironmentProperties.java
@@ -1,14 +1,20 @@
 package com.rbkmoney.threeds.server.config.properties;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class EnvironmentProperties {
 
+    @Length(max = 2048)
     private String dsUrl;
+    @Length(max = 2048)
     private String threeDsRequestorUrl;
+    @Length(max = 2048)
     private String threeDsServerUrl;
+    @Length(max = 32)
     private String threeDsServerRefNumber;
+    @Length(max = 32)
     private String threeDsServerOperatorId;
     private int threeDsServerNetworkTimeout;
 
